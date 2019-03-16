19ece3b515c9d8eb9d42f8f216951ed9b00653f0
ardyh <ardyharoen@gmail.com>
2019-03-16 22:52:48 +0700

    a

--- header/FarmAnimal.h
+++ header/FarmAnimal.h
@@ -7,11 +7,15 @@ class FarmAnimal {
         Point lokasi;
         string nama;
         string suara;
+        int waktuLapar;
+        bool lapar;
 
     public:
         //KONSTRUKTOR
-        FarmAnimal(Point, nama, suara);
+        FarmAnimal(Point, string, string); 
         
         void gerak();
         void bersuara();
+        void mati();
+        void makan();
 };
