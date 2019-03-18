6d9fbc0b13a2da697fb95c9c9f3dfb0e1bcf7399
Taufikurrahman Anwar <16517087@office.itb.ac.id>
2019-03-18 20:47:13 +0700

    Side Product creation

--- header/SideProduct.h
+++ header/SideProduct.h
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
+#include "Product.h"
+#include "Player.h"
 using namespace std;
 
-class SideProduct {
+class SideProduct : public Product{
     private:
-    
+        string *requirements;
     public:
+    /* Jika ingin membuat SideProduct, uruttan langkahnya:
+            - Create new object "SideProduct prod;"
+            - Ask if obtainable "if (prod.obtainable())" 
+            - Assign prod with a new object "prod = new SideProduct(*this)"*/
+        SideProduct(Player user);
+        SideProduct& operator =(SideProduct input);
+        bool obtainable(Player user);
 };
