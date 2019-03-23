4f7d6481e68d0d6685a6f9528e10d4f11ba9de7f
Frankuy <fhizbullah99@gmail.com>
2019-03-23 19:42:18 +0700

    delete file

--- header/Renderable.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDERABLE_H
-#define RENDERABLE_H
-class Renderable {
-    //Renderable ialah Abstract Class Untuk membantu dalam menampilkan/mencetak representasi dari objek 
-    public:
-        virtual char render() = 0;
-};
-#endif
